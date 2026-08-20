e141c72c13ea0751f6d5b06c993c54292e9f24e1
MadhanRajG <madhanrajmaddy007@gmail.com>
2026-08-20 12:20:37 +0530

    Add files via upload

---
+++ Modules/3/generate_hospital_kpis.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+df = pd.read_excel("hospital_final_dataset.xlsx")
+
+print("\n===== HOSPITAL KPI REPORT =====\n")
+
+# KPI 1 - Total Admissions
+total_admissions = len(df)
+
+# KPI 2 - Average Length of Stay
+avg_los = round(df["Length_of_stay"].mean(), 2)
+
+# KPI 3 - Readmission Rate
+readmitted = (df["Readmission_Flag"] == "Yes").sum()
+readmission_rate = round((readmitted / len(df)) * 100, 2)
+
+# KPI 4 - Occupancy Rate
+occupied = (df["Occupancy_Status"] == "Occupied").sum()
+occupancy_rate = round((occupied / len(df)) * 100, 2)
+
+# KPI 5 - Bed Utilization Rate
+beds_used = df["Bed_ID"].nunique()
+total_beds = df["Bed_Capacity"].sum()
+
+bed_utilization_rate = round((beds_used / total_beds) * 100, 2)
+
+# KPI 6 - Department Efficiency Score
+
+dept_avg = df.groupby("Department")["Length_of_stay"].mean()
+
+dept_efficiency = (
+    (dept_avg.max() - dept_avg)
+    / (dept_avg.max() - dept_avg.min())
+    * 40 + 60
+).round(2)
+
+dept_efficiency = dept_efficiency.sort_values(ascending=False)
+
+print("Total Admissions:", total_admissions)
+print("Average Length of Stay:", avg_los, "Days")
+print("Readmission Rate:", readmission_rate, "%")
+print("Occupancy Rate:", occupancy_rate, "%")
+print("Bed Utilization Rate:", bed_utilization_rate, "%")
+
+print("\n===== Department Efficiency =====\n")
+print(dept_efficiency)
+
+print("\nKPI Engineering Completed Successfully")
+kpi_report = pd.DataFrame({
+    "KPI": [
+        "Total Admissions",
+        "Average Length of Stay",
+        "Readmission Rate",
+        "Occupancy Rate",
+        "Bed Utilization Rate"
+    ],
+    "Value": [
+        total_admissions,
+        avg_los,
+        readmission_rate,
+        occupancy_rate,
+        bed_utilization_rate
+    ]
+})
+
+kpi_report.to_csv("hospital_kpi_report.csv", index=False)
+
+print("KPI Report Saved Successfully")
